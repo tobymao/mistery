9b0ad52e534040b3cf27d2165a318c9a5ec12a16
Toby Mao <toby.mao@gmail.com>
2015-02-26 20:46:58 -0800

    added minimal text

--- app/views/plays/index.rb
+++ app/views/plays/index.rb
@@ -6,18 +6,19 @@ class Views::Plays::Index < Views::Layouts::Page
   def main
     h1 'Play a Scenario'
 
+    text "Active Games"
     active_plays.each do |play|
       div do
         link_to play.scenario.name, play, class: 'mainLink'
       end
     end
 
+    text "Start A New Game"
     plays.each do |play|
       form_for play do |f|
         f.hidden_field :scenario_id, value: play.scenario.id
         f.button play.scenario.name, class: 'mainLink'
       end
     end
-
   end
 end
