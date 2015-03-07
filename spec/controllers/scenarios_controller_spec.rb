abc20bea1f253e7d753cf7beac0a82342d15ad0e
Toby Mao <toby.mao@gmail.com>
2015-03-07 00:35:34 -0800

    start scenario spec

--- spec/controllers/scenarios_controller_spec.rb
+++ spec/controllers/scenarios_controller_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 describe ScenariosController do
+  let(:user) {create(:user)}
+  let(:authenticate) {allow(controller).to receive(:current_user).and_return(user)}
+
+  describe "GET index" do
+    context 'unauthenticated' do
+      before :each do
+        get :index
+      end
+
+      it {expect(response).to be_success}
+    end
+
+    context 'authenticated' do
+      before :each do
+        authenticate
+        get :index
+      end
+
+      it {expect(response).to be_success}
+    end
+  end
 end
