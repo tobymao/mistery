c81faf13b4b1a6908a058798ada9287ac15113a9
Toby Mao <toby.mao@gmail.com>
2015-03-09 22:08:30 -0700

    show spec

--- spec/controllers/scenarios_controller_spec.rb
+++ spec/controllers/scenarios_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe ScenariosController do
   let(:user) {create(:user)}
   let(:authenticate) {allow(controller).to receive(:current_user).and_return(user)}
+  let(:scenario) {create(:scenario, user: user)}
 
   describe "GET index" do
     context 'unauthenticated' do
@@ -17,6 +18,25 @@ describe ScenariosController do
       before :each do
         authenticate
         get :index
+      end
+
+      it {expect(response).to be_success}
+    end
+  end
+
+  describe "GET show" do
+    context 'unauthenticated' do
+      before :each do
+        get :show, {id: scenario.id}
+      end
+
+      it {expect(response).to be_success}
+    end
+
+    context 'authenticated' do
+      before :each do
+        authenticate
+        get :show, {id: scenario.id}
       end
 
       it {expect(response).to be_success}
