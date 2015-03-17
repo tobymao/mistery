266b367bd2504c8f838bf4b2fdf2e4479be36399
Toby Mao <toby.mao@gmail.com>
2015-03-16 21:36:09 -0700

    fixed universe order

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,16 +4,9 @@ class LandingController < ApplicationController
 
   # GET /
   def index
-    scenarios = Scenario
-      .includes(:user)
-      .published
-      .last(NEW_SCENARIO_LIMIT)
-
+    scenarios = Scenario.includes(:user).published.last(NEW_SCENARIO_LIMIT)
     @scenarios_new = scenarios.last(10).reverse
     @scenarios_top = scenarios.sort_by(&:plays_count).reverse.take(10)
-    @universes_new = Universe
-      .includes(:user)
-      .published
-      .last(NEW_UNIVERSE_LIMIT)
+    @universes_new = Universe.includes(:user).published.last(NEW_UNIVERSE_LIMIT).reverse
   end
 end
