c98670ba60e408f314d6619c86ac59c792ae5d5e
colinscampbell <cscampbell@gmail.com>
2015-03-16 23:43:54 -0500

    user profile emails private

--- app/views/users/edit.rb
+++ app/views/users/edit.rb
@@ -1,7 +1,30 @@
 class Views::Users::Edit < Views::Layouts::Page
-  needs :user
+needs :current_user
 
   def main
     h1 "Edit User"
+
+    form_tag user_path(current_user), method: 'PUT' do
+      fields_for 'current_user' do |f|
+
+        f.label :email, 'Your Email'
+        f.text_field :email
+
+        f.submit 'Update Email'
+      end
+    end
+
+    #text_field asdf
+
+    form_tag user_path(current_user), method: 'PUT' do
+      fields_for 'current_user' do |f|
+
+        f.label :password, 'Your Password'
+        f.text_field :password
+
+        f.submit 'Update Password'
+      end
+    end
+
   end
 end
--- app/views/users/show.rb
+++ app/views/users/show.rb
@@ -1,26 +1,30 @@
 class Views::Users::Show < Views::Layouts::Page
   needs :user
+  needs :current_user
 
   def main
     h1 "User Profile"
     link_to "Edit Profile", edit_user_path(user)
 
     p do
-      text "Your Username: "
+      text "Username: "
       text user.login
     end
 
+
     p do
-    	text "Your Email: "
-    	text user.email
-    	text " "
-    	
+      if(user == current_user)
+        text "Your Email: "
+        text user.email
+        text " "	
+      else
+        text "Email Private"
+      end
     end
 
     p do
-    	text "You have been a member for "
-    	text (Date.today - user.created_at.to_date).to_i
-    	text " days"
+    	text "Member since "
+    	text user.created_at.to_date
     end
 
 
