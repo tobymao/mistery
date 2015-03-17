b5a803ea862d67a92aa15a0a3681e181901e3c07
colinscampbell <cscampbell@gmail.com>
2015-03-16 20:31:53 -0500

    minor profile changes

--- app/views/users/show.rb
+++ app/views/users/show.rb
@@ -3,9 +3,27 @@ class Views::Users::Show < Views::Layouts::Page
 
   def main
     h1 "User Profile"
+    link_to "Edit Profile", edit_user_path(user)
 
     p do
+      text "Your Username: "
       text user.login
     end
+
+    p do
+    	text "Your Email: "
+    	text user.email
+    	text " "
+    	
+    end
+
+    p do
+    	text "You have been a member for "
+    	text (Date.today - user.created_at.to_date).to_i
+    	text " days"
+    end
+
+
+
   end
 end
