7c3b658732777fa9fd0c0c3f4fe655858dd54969
colinscampbell <cscampbell@gmail.com>
2015-03-30 18:27:15 -0500

    adjusted tests, removed extra files

--- spec/mailers/login_help_spec.rb
+++ spec/mailers/login_help_spec.rb
@@ -3,31 +3,32 @@ require "rails_helper"
 RSpec.describe LoginHelp, type: :mailer do
   let(:user) { create(:user, login: "test", password: "password", email: "test@example.com")}
 
+
   describe "username" do
-    let(:mail) { LoginHelp.username(:user) }
+    let(:mail) { LoginHelp.username_email(:user) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Mistery.io Username")
-      expect(mail.to).to eq([user.email])
+      expect(mail.to).to eq(["test@example.com"])
       #expect(mail.from).to eq(["TBD@mistery.io"]) #Add from email when mail server emails are finalized
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Your Username is #{user.login}")
+      expect(mail.body.encoded).to match("Your Username is test")
     end
   end
 
   describe "password" do
-    let(:mail) { LoginHelp.password(:user) }
+    let(:mail) { LoginHelp.password_email(:user) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Mistery.io Password")
-      expect(mail.to).to eq([user.email])
+      expect(mail.to).to eq(["test@example.com"])
       #expect(mail.from).to eq(["TBD@mistery.io"]) #Add from email when mail server emails are finalized
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Your Username is #{user.password}")
+      expect(mail.body.encoded).to match("Your Password is password")
     end
   end
 
