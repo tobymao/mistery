c0f36dc6b2c0de8825cbd49ae1f984148f25f8d9
Toby Mao <toby.mao@gmail.com>
2015-04-06 22:07:12 -0700

    fixed specs

--- spec/factories/password_resets.rb
+++ spec/factories/password_resets.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :password_reset do
-    
+    association :user
+    token "token"
   end
-
 end
--- spec/mailers/login_help_spec.rb
+++ spec/mailers/login_help_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe LoginHelp, type: :mailer do
-  let(:user) { create(:user, login: "test", password: "password", email: "test@example.com")}
-
+  let(:user) {create(:user, login: "test", password: "password", email: "test@example.com")}
+  let(:password_reset) {create(:password_reset)}
 
   describe "username" do
     let(:mail) { LoginHelp.username_email(user) }
@@ -10,6 +10,7 @@ RSpec.describe LoginHelp, type: :mailer do
     it "renders the headers" do
       expect(mail.subject).to eq("Mistery.io Username")
       expect(mail.to).to eq(["test@example.com"])
+      expect(mail.from).to eq(["no-reply@mistery.io"])
     end
 
     it "renders the body" do
@@ -18,15 +19,16 @@ RSpec.describe LoginHelp, type: :mailer do
   end
 
   describe "password" do
-    let(:mail) { LoginHelp.password_email(user) }
+    let(:mail) { LoginHelp.password_email(user, password_reset) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Mistery.io Password")
       expect(mail.to).to eq(["test@example.com"])
+      expect(mail.from).to eq(["no-reply@mistery.io"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to start_with("Your password has been reset. Your new temporary password is ")
+      expect(mail.body.encoded).to start_with("Click this link to reset your password.")
     end
   end
 
