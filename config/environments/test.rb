d48a0bfdb7b45cc8d9b2bddab860c9857aa4b7db
colinscampbell <cscampbell@gmail.com>
2015-04-06 21:35:21 -0500

    fixed comments and login instance variables

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,7 +32,10 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.delivery_method = :test
 
 end
